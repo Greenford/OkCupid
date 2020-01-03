15e22afa23c95c7002befff7f7c5e01e57bee0cd
Greenford <lauren.n.greenford@gmail.com>
2020-01-02 20:54:12 -0600

    actually added scrape.py this time

---
+++ scrape.py
@@ -0,0 +1,21 @@
+from okc_scraper_controller import Scraper
+import pandas as pd
+
+if __name__=='__main__':
+    scrapername = pd.read_csv('src/okc_account_credentials', index_col=0).iloc[0].name
+
+    scraper = Scraper(scrapername)
+    scraper.login()
+    print('login successful')
+    qd = scraper.get_scraper_question_data()
+    print('retrieved inital question data')
+    scraper.set_first_version(qd)
+    print('set first version')
+
+    qd = scraper.answer_all_questions()
+    print('all questions answered')
+    scraper.add_questions_update_version(qd)
+    print('version updated')
+
+    scraper.logout()
+    print('logged out')
